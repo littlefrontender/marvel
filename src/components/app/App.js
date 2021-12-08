5e72af67e438146be65182c327285d773791ee01
littlefrontender <you@example.com>
2021-12-08 17:14:06 +0300

    some build

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -10,7 +10,7 @@ const App = () => {
                 <AppHeader/>
                 <main>
                     <Routes>
-                        <Route path='/' element={<MainPage/>} />
+                        <Route path='/marvel' element={<MainPage/>} />
                         <Route path='/comics' element={<ComicsPage/>} />
                         <Route path='/comics/:comicId' element={<SingleComicPage/>} />
                         <Route path='*' element={<Page404/>} />
